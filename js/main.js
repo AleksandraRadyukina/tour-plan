f7268f22ba3aa5b76c22d14f6dafde6a1d653658
AleksandraRadyukina <RadyukinaAleksandra@gmail.com>
2021-05-13 15:09:16 +0300

    create: added img in slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 const swiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
+  keyboard: {
+    enabled: true,
+  },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.slider-button_novigate_next',
+    prevEl: '.slider-button_novigate_prev',
   },
 });
